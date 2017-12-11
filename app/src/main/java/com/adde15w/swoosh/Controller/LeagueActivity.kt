fc476377f44f2ba7f59701d18c609b4014dc90af
Adrian Wieslander <adde15w@hotmail.com>
2017-12-11 14:00:49 +0100

    Swoosh complete - passing data from different views

---
+++ app/src/main/java/com/adde15w/swoosh/Controller/LeagueActivity.kt
@@ -0,0 +1,53 @@
+package com.adde15w.swoosh.Controller
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.adde15w.swoosh.Model.Player
+import com.adde15w.swoosh.R
+import com.adde15w.swoosh.Utilities.EXTRA_PLAYER
+import kotlinx.android.synthetic.main.activity_league.*
+
+class LeagueActivity : BaseActivity() {
+
+    var player = Player("", "")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+
+    fun onMensClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        player.league = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        player.league = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        player.league = "coed"
+    }
+
+    fun leagueNextClicked(view: View) {
+        if (player.league != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
+            startActivity(skillActivity)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/adde15w/swoosh/Utilities/Constans.kt
@@ -0,0 +1,7 @@
+package com.adde15w.swoosh.Utilities
+
+/**
+ * Created by Karl-Johan on 07-12-2017.
+ */
+
+const val EXTRA_PLAYER = "player"
